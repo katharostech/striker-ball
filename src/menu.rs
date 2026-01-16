8b7fb63f2d7d82986daffa96d84d3bd86ed67536
Tekhnae Raav <tekhnaeraav@katharostech.com>
2026-01-16 13:41:28 -0600

    care: move network file into flow

--- src/menu.rs
+++ src/menu.rs
@@ -5,8 +5,3 @@ pub use plugin::*;
 
 mod flow;
 pub use flow::*;
-
-#[cfg(not(target_arch = "wasm32"))]
-mod network;
-#[cfg(not(target_arch = "wasm32"))]
-pub use network::*;
--- src/menu/flow.rs
+++ src/menu/flow.rs
@@ -99,7 +99,7 @@ pub fn update_menu(world: &World) {
                 .process_input(world)
                 .or(network_quit)
             {
-                network_quit_transition(world, output)
+                network::network_quit_transition(world, output)
             }
         }
         #[cfg(not(target_arch = "wasm32"))]
@@ -109,7 +109,7 @@ pub fn update_menu(world: &World) {
                 .process_input(world)
                 .or(lan_select)
             {
-                lan_select_transition(world, output);
+                network::lan_select_transition(world, output);
             }
         }
         #[cfg(not(target_arch = "wasm32"))]
@@ -118,7 +118,7 @@ pub fn update_menu(world: &World) {
                 .resource_mut::<LanUI>()
                 .process_input(world)
                 .or(lan_ui);
-            lan_ui_transition(world, output)
+            network::lan_ui_transition(world, output)
         }
     }
 }
@@ -381,57 +381,151 @@ pub fn pause_transition(world: &World, output: PauseOutput) {
     }
 }
 
-pub fn network_quit_transition(world: &World, output: NetworkQuitOutput) {
-    match output {
-        NetworkQuitOutput::Quit => {
+#[cfg(not(target_arch = "wasm32"))]
+pub mod network {
+    use super::*;
+
+    pub fn lan_ui_hide(world: &World) {
+        world.resource_mut::<Matchmaker>().disable_search();
+        world.resource_mut::<LanUI>().visible = false;
+    }
+    pub fn lan_ui_leave(world: &World) {
+        world.resource_mut::<Matchmaker>().lan_cancel();
+        world.resource_mut::<Matchmaker>().disable_search();
+        world.resource_mut::<LanUI>().visible = false;
+    }
+    pub fn lan_ui_prep(world: &World) {
+        world.resource_mut::<Matchmaker>().enable_search();
+        world.resource_mut::<LanUI>().visible = true;
+    }
+    pub fn play_online_prep(ui: &World) {
+        let socket = ui.resource::<Matchmaker>().network_match_socket().unwrap();
+        let service_type = *ui.resource::<ServiceType>();
+        let mut sessions = ui.resource_mut::<Sessions>();
+
+        sessions.create_play(PlayMode::Online {
+            socket,
+            service_type,
+        });
+        *ui.resource_mut() = MenuState::InNetworkGame;
+    }
+    pub fn lan_select_transition(world: &World, output: LanSelectOutput) {
+        match output {
+            LanSelectOutput::Exit => {
+                start_fade(
+                    world,
+                    FadeTransition {
+                        hide: LanSelect::hide_resource,
+                        prep: Splash::show_resource,
+                        finish: MenuState::Splash,
+                    },
+                );
+            }
+            LanSelectOutput::ServiceType(service) => {
+                world.resource_mut::<LanUI>().service = service;
+                start_fade(
+                    world,
+                    FadeTransition {
+                        hide: LanSelect::show_resource,
+                        prep: lan_ui_prep,
+                        finish: MenuState::Lan,
+                    },
+                );
+            }
+        }
+    }
+    pub fn lan_ui_transition(world: &World, output: Option<LanUIOutput>) {
+        let lan_ui = world.resource::<LanUI>();
+        let mut matchmaker = world.resource_mut::<Matchmaker>();
+
+        if matchmaker.network_match_socket().is_some() {
+            world.resources.insert(lan_ui.service);
             start_fade(
                 world,
                 FadeTransition {
-                    hide: play_leave,
+                    hide: lan_ui_hide,
+                    prep: play_online_prep,
+                    finish: MenuState::InNetworkGame,
+                },
+            );
+            return;
+        }
+        let Some(output) = output else { return };
+        match output {
+            LanUIOutput::HostCancel => {
+                if matchmaker.is_waiting() {
+                    matchmaker.lan_cancel();
+                } else {
+                    matchmaker.lan_host();
+                }
+            }
+            LanUIOutput::Server(i) => {
+                if let Some(server) = matchmaker.lan_servers().get(i).cloned() {
+                    matchmaker.lan_join(&server);
+                }
+            }
+            LanUIOutput::Exit => start_fade(
+                world,
+                FadeTransition {
+                    hide: lan_ui_leave,
                     prep: Splash::show_resource,
                     finish: MenuState::Splash,
                 },
-            );
+            ),
         }
-        NetworkQuitOutput::Show => {
-            let mut sessions = world.resource_mut::<Sessions>();
-            let session = sessions.get_mut(session::PLAY).unwrap();
-            session.world.resource_mut::<Countdown>().visual.add_hide();
-            session.world.resource_mut::<MatchDone>().visual.add_hide();
-            session
-                .world
-                .resource_mut::<ScoreDisplay>()
-                .visual
-                .add_hide();
-            session
-                .world
-                .resource_mut::<WinnerBanner>()
-                .visual
-                .add_hide();
-        }
-        NetworkQuitOutput::Hide => {
-            let mut sessions = world.resource_mut::<Sessions>();
-            let session = sessions.get_mut(session::PLAY).unwrap();
-            session
-                .world
-                .resource_mut::<Countdown>()
-                .visual
-                .remove_hide();
-            session
-                .world
-                .resource_mut::<MatchDone>()
-                .visual
-                .remove_hide();
-            session
-                .world
-                .resource_mut::<ScoreDisplay>()
-                .visual
-                .remove_hide();
-            session
-                .world
-                .resource_mut::<WinnerBanner>()
-                .visual
-                .remove_hide();
+    }
+    pub fn network_quit_transition(world: &World, output: NetworkQuitOutput) {
+        match output {
+            NetworkQuitOutput::Quit => {
+                start_fade(
+                    world,
+                    FadeTransition {
+                        hide: play_leave,
+                        prep: Splash::show_resource,
+                        finish: MenuState::Splash,
+                    },
+                );
+            }
+            NetworkQuitOutput::Show => {
+                let mut sessions = world.resource_mut::<Sessions>();
+                let session = sessions.get_mut(session::PLAY).unwrap();
+                session.world.resource_mut::<Countdown>().visual.add_hide();
+                session.world.resource_mut::<MatchDone>().visual.add_hide();
+                session
+                    .world
+                    .resource_mut::<ScoreDisplay>()
+                    .visual
+                    .add_hide();
+                session
+                    .world
+                    .resource_mut::<WinnerBanner>()
+                    .visual
+                    .add_hide();
+            }
+            NetworkQuitOutput::Hide => {
+                let mut sessions = world.resource_mut::<Sessions>();
+                let session = sessions.get_mut(session::PLAY).unwrap();
+                session
+                    .world
+                    .resource_mut::<Countdown>()
+                    .visual
+                    .remove_hide();
+                session
+                    .world
+                    .resource_mut::<MatchDone>()
+                    .visual
+                    .remove_hide();
+                session
+                    .world
+                    .resource_mut::<ScoreDisplay>()
+                    .visual
+                    .remove_hide();
+                session
+                    .world
+                    .resource_mut::<WinnerBanner>()
+                    .visual
+                    .remove_hide();
+            }
         }
     }
 }
